d9a39a69fd55d7999daccb3859675a0154dee3b8
SayToMe <kkkristal@mail.ru>
2015-03-24 03:02:22 +0300

    Initial scaffolding commit.

---
+++ src/Frolog/Library.fs
@@ -0,0 +1,16 @@
+namespace Frolog
+
+/// Documentation for my library
+///
+/// ## Example
+///
+///     let h = Library.hello 1
+///     printfn "%d" h
+///
+module Library = 
+  
+  /// Returns 42
+  ///
+  /// ## Parameters
+  ///  - `num` - whatever
+  let hello num = 42
---
+++ tests/Frolog.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module Frolog.Tests
+
+open Frolog
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
